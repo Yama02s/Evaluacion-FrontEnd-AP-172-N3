9ba07718385bf6b7491568894ad6eae48d2018a3
Yama02s <127345979+Yama02s@users.noreply.github.com>
2025-04-24 22:39:26 -0400

    Add files via upload

---
+++ GSPMusic/JS/GsP.js
@@ -0,0 +1,12 @@
+function enviarMensaje(){
+
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const mensaje = document.getElementById("mensaje").value;
+
+    
+    if(nombre,email,mensaje) {
+        alert(`gracias ${nombre} por contactarnos`)
+        this.reset()
+    }  
+}
